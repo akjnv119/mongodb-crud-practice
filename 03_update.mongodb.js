46f79e37d19792bf201ccbc9e5315cd3f47194d2
Aman Kumar <akjnv119@gmail.com>
2026-01-09 22:40:17 +0530

    initial commit of MongoDB update script with example queries

---
+++ 03_update.mongodb.js
@@ -0,0 +1,22 @@
+use('ecommerce')
+
+
+//db.products.find()
+// db.products.updateOne(
+// { name: "Wireless Mouse" },
+// { $set: { price: 899 } }
+// )
+//db.products.find()
+// db.products.updateMany(
+// { category: "Electronics" },
+// { $inc: { stock: 10 } }
+// )
+
+// db.products.find()
+
+// db.products.updateOne(
+// { name: "Wireless Mouse" },
+// { $push: { tags: "new" } }
+// )
+
+db.products.find()
